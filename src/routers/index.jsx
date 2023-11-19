ce0c6c4027f91149d9ec2e2bddefb0e4c6144777
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-11-19 09:58:44 +0700

    chore: implement error boundary

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,6 +1,10 @@
+import { Suspense } from "react";
 import { createBrowserRouter, redirect, Outlet } from "react-router-dom";
-import { AuthLoginPage, AuthRegisterPage } from "@/pages";
+import { lazily } from "react-lazily";
 import { TokenService } from "@/libs";
+import { ErrorBoundary } from "react-error-boundary";
+
+const { AuthLoginPage, AuthRegisterPage } = lazily(() => import("@/pages"));
 
 export const routes = createBrowserRouter([
   {
@@ -9,7 +13,14 @@ export const routes = createBrowserRouter([
   },
   {
     path: "/auth",
-    element: <Outlet />,
+    element: (
+      <ErrorBoundary fallback={<div>Error</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </ErrorBoundary>
+    ),
+    errorElement: <div>Error</div>,
     children: [
       {
         path: "/auth/login",
